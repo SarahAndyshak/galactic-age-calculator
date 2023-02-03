0d80bb6e25ed484f33e982925189fc6d008e4c11
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-02-03 15:49:41 -0800

    Add birthday-calc.js and birthday-calc.js

---
+++ __tests__/birthday-calc.test.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+import BirthdayCount from '../src/js/birthday-calc';
+
+describe ('BirthdayCount', () => {
+  test('should create birthday object for the current date', () => {
+    const birthday = new Birthday();
+    expect(birthday.currentDay).toEqual();
+  });
+});
---
+++ src/js/birthday-calc.js
@@ -0,0 +1,15 @@
+export default class BirthdayCount {
+
+}
+
+// export default class Age {
+//   constructor(earthAge, pastAge, futureAge) {
+//     this.earthAge = earthAge;
+//     this.pastAge = pastAge;
+//     this.futureAge = futureAge;
+//     this.mercuryAge = (this.earthAge / .24).toFixed(2);
+//     this.venusAge = (this.earthAge / .62).toFixed(2);
+//     this.marsAge = (this.earthAge / 1.88).toFixed(2);
+//     this.jupiterAge = (this.earthAge / 11.86).toFixed(2);
+//     this.earthPast = (this.earthAge - pastAge).toFixed(2);
+//   }
