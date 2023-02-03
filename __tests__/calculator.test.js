e8900d513fdce2b1b940b0a84bfe3a36ada87d86
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-02-03 09:53:20 -0800

    Refactor so that previous prototypes become constructor keys so that future prototypes should be easier. Save all previous tests and prototypes commmented out in case.

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -7,25 +7,33 @@ describe ('Age', () => {
     expect(age.earthAge).toEqual(5);
   });
 
-  test('should return a users age in Mercury years', () => {
+  test('should create an age object for a users age in Earth years and Mercury years', () => {
     const age = new Age(5);
-    expect(age.mercuryAge()).toEqual(21);
+    expect(age.earthAge).toEqual(5);
+    expect(age.mercuryAge).toEqual(21);
   });
 
-  test('should return a users age in Venus years', () => {
-    const age = new Age(5);
-    expect(age.venusAge()).toEqual(8);
-  });
+  // test('should return a users age in Mercury years', () => {
+  //   const age = new Age(5);
+  //   expect(age.mercuryAge()).toEqual(21);
+  // });
 
-  test('should return a users age in Mars years', () => {
-    const age = new Age(5);
-    expect(age.marsAge()).toEqual(9);
-  });
+  // test('should return a users age in Venus years', () => {
+  //   const age = new Age(5);
+  //   expect(age.venusAge()).toEqual(8);
+  // });
 
-  test('should return a users age in Jupiter years', () => {
-    const age = new Age(5);
-    expect(age.jupiterAge()).toEqual(59);
-  });
+  // test('should return a users age in Mars years', () => {
+  //   const age = new Age(5);
+  //   expect(age.marsAge()).toEqual(9);
+  // });
+
+  // test('should return a users age in Jupiter years', () => {
+  //   const age = new Age(5);
+  //   expect(age.jupiterAge()).toEqual(59);
+  // });
+
+
 
 });
 
--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,21 +1,44 @@
 export default class Age {
   constructor(earthAge) {
     this.earthAge = earthAge;
+    this.mercuryAge = Math.round(this.earthAge / .24);
   }
 
-  mercuryAge() {
-    return Math.round(this.earthAge / .24);
-  }
+  // mercuryAge() {
+  //   return Math.round(this.earthAge / .24);
+  // }
 
-  venusAge() {
-    return Math.round(this.earthAge / .62);
-  }
+  // venusAge() {
+  //   return Math.round(this.earthAge / .62);
+  // }
 
-  marsAge(){
-    return Math.round(this.earthAge * 1.88);
-  }
+  // marsAge(){
+  //   return Math.round(this.earthAge * 1.88);
+  // }
 
-  jupiterAge(){
-    return Math.round(this.earthAge * 11.86);
-  }
+  // jupiterAge(){
+  //   return Math.round(this.earthAge * 11.86);
+  // }
 }
+
+// export default class Age {
+//   constructor(earthAge) {
+//     this.earthAge = earthAge;
+//   }
+
+//   mercuryAge() {
+//     return Math.round(this.earthAge / .24);
+//   }
+
+//   venusAge() {
+//     return Math.round(this.earthAge / .62);
+//   }
+
+//   marsAge(){
+//     return Math.round(this.earthAge * 1.88);
+//   }
+
+//   jupiterAge(){
+//     return Math.round(this.earthAge * 11.86);
+//   }
+// }
