915df68dd79140a122ffd3954ded797a8eede862
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-02-03 15:56:48 -0800

    Create class BirthdayCount and receive good fail test connecting birthday-calc.js to birthday-calc.test.js.

--- __tests__/birthday-calc.test.js
+++ __tests__/birthday-calc.test.js
@@ -3,7 +3,7 @@ import BirthdayCount from '../src/js/birthday-calc';
 
 describe ('BirthdayCount', () => {
   test('should create birthday object for the current date', () => {
-    const birthday = new Birthday();
-    expect(birthday.currentDay).toEqual();
+    const birthdayCount = new BirthdayCount("Fri Feb 03 2023");
+    expect(birthdayCount.currentDay).toEqual("Fri Feb 03 2023");
   });
 });
--- src/js/birthday-calc.js
+++ src/js/birthday-calc.js
@@ -1,5 +1,5 @@
 export default class BirthdayCount {
-
+  
 }
 
 // export default class Age {
