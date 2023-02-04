a20564645e90bda035fb6f57afbde52dafd6bf4e
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-02-03 16:12:33 -0800

    Good fail for setCurrentDay prototype test.

--- __tests__/birthday-calc.test.js
+++ __tests__/birthday-calc.test.js
@@ -2,8 +2,14 @@
 import BirthdayCount from '../src/js/birthday-calc';
 
 describe ('BirthdayCount', () => {
+  
   test('should create birthday object for the current date', () => {
     const birthdayCount = new BirthdayCount("Fri Feb 03 2023");
     expect(birthdayCount.currentDay).toEqual("Fri Feb 03 2023");
+  });
+
+  test('should create set the current day based on time of user input', () => {
+    const birthdayCount = new BirthdayCount("Fri Feb 03 2023");
+    expect(birthdayCount.setCurrentDay()).toEqual("Fri Feb 03 2023");
   });
 });
--- src/js/birthday-calc.js
+++ src/js/birthday-calc.js
@@ -2,16 +2,8 @@ export default class BirthdayCount {
   constructor(currentDay){
     this.currentDay = currentDay;
   }
-}
 
-// export default class Age {
-//   constructor(earthAge, pastAge, futureAge) {
-//     this.earthAge = earthAge;
-//     this.pastAge = pastAge;
-//     this.futureAge = futureAge;
-//     this.mercuryAge = (this.earthAge / .24).toFixed(2);
-//     this.venusAge = (this.earthAge / .62).toFixed(2);
-//     this.marsAge = (this.earthAge / 1.88).toFixed(2);
-//     this.jupiterAge = (this.earthAge / 11.86).toFixed(2);
-//     this.earthPast = (this.earthAge - pastAge).toFixed(2);
-//   }
+setCurrentDay() {
+  
+}
+}
