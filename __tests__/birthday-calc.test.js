062453f9da89538055990e161c25ceaeedf5b37c
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-02-03 16:40:48 -0800

    good fail for setBirthday prototype test.

--- __tests__/birthday-calc.test.js
+++ __tests__/birthday-calc.test.js
@@ -13,13 +13,8 @@ describe ('BirthdayCount', () => {
     expect(birthdayCount.setCurrentDay()).toEqual("2/3/2023");
   });
 
-  // test('should create set the current day based on time of user input', () => {
-  //   const birthdayCount = new BirthdayCount("Fri Feb 03 2023");
-  //   expect(birthdayCount.setCurrentDay()).toEqual("Fri Feb 03 2023");
-  // });
-
-  // test('should capture the user birthday input', () => {
-  //   const birthdayCount = new BirthdayCount("Fri Feb 03 2023");
-  //   expect(birthdayCount.setBirthday()).toEqual("");
-  // });
+  test('should capture the user birthday input', () => {
+    const birthdayCount = new BirthdayCount("2/3/2023");
+    expect(birthdayCount.setBirthday()).toEqual("6/14/1981");
+  });
 });
